b01bfd96b28d67d0ff06d269a12a5199172e42c6
pro-roshdy <pro@bloggybro.cc>
2022-02-12 22:38:18 +0200

    1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ function myFunction() {
     bodyDark.classList.toggle("dark-mode");
     var haeder=document.getElementById("haeder");
     haeder.classList.toggle("header-dark-mode");
-    let container =document.getElementById("con-dark")
-    container.classList.toggle("con-dark")
+    let container =document.getElementById("con-dark");
+    container.classList.toggle("con-dark");
+    let headerH1 = document.getElementById("h1-dark");
+    headerH1.classList.toggle("HHH1");
+    let containerh1 = document.getElementById("hh1-dark");
+    containerh1.classList.toggle("continerh4")
 }
