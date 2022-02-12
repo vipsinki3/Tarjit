ca7208d96c595bd4f9c85bb8ca6bfbe950d89283
pro-roshdy <pro@bloggybro.cc>
2022-02-12 21:40:17 +0200

    up

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-let myElement = document.getElementById("hhh");
-let continer = document.getElementById("sss");
-let haeder = document.getElementById("haeder");
-let sora = document.getElementById("sora");
-// let h2 = document.getElementById("h2");
-
-const change = () => {
-    myElement.style.backgroundColor= "#272727";
-    haeder.style.backgroundColor= "#272727";
-    continer.style.backgroundColor= "rgb(46 46 46)";
-    // h2.style.boxshadow = "0px 0px 18px #0075ff";
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var haeder=document.getElementById("haeder");
+    haeder.classList.toggle("header-dark-mode");
+    let container =document.getElementById("con-dark")
+    container.classList.toggle("con-dark")
 }
-sora.onclick = change;
